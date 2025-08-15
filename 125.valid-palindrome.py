b2e61656e217980620517ae5dee15b59ad92670a
Tarun Chauhan <hifidevil999@gmail.com>
2025-08-15 21:16:24 +0530

    feat: add solution for LeetCode problem 125 - Valid Palindrome

---
+++ 125.valid-palindrome.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = ''.join(char for char in s if char.isalnum())
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+# @lc code=end
+
