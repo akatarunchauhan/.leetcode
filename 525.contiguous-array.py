6be5e07fd6c7bad963218705c48bb28812837655
Tarun Chauhan <hifidevil999@gmail.com>
2025-09-14 20:15:47 +0530

    feat: add solution for LeetCode problem 525 - Contiguous Array

---
+++ 525.contiguous-array.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=525 lang=python3
+#
+# [525] Contiguous Array
+#
+
+# @lc code=start
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        one , zero = 0,0
+        res = 0
+        diff_index = {}
+        
+        for i,n in enumerate(nums):
+            if n == 0:
+                zero+=1
+            else:
+                one+=1
+            if one-zero not in diff_index:
+                diff_index[one-zero] = i
+            if one==zero:
+                res = one+zero
+            else:
+                index = diff_index[one-zero]
+                res= max(res,i-index)
+        return res
+# @lc code=end
+
